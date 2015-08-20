a2b519ff3bf390db0356bb31aa4683e7480cecc3
Alex Costa & Jordan Johansen <student@epicodus.com>
2015-08-20 15:07:03 -0700

    Add delete all cuisines button to index.html.twig.

--- app/app.php
+++ app/app.php
@@ -19,17 +19,43 @@
     ));
 
 
-    $app->get("/", function() use ($app){
+    //Home
+    $app->get("/", function() use ($app) {
         return $app['twig']->render('index.html.twig', array('cuisines' => Cuisine::getAll(), 'cuisine_check' => false, 'venue_check' => false));
     });
 
-    $app->get("/form-cuisines", function() use ($app){
+    //Cuisine form view
+    $app->get("/form-cuisines", function() use ($app) {
         return $app['twig']->render('index.html.twig', array('cuisines' => Cuisine::getAll(), 'cuisine_check' => true, 'venue_check' => false));
     });
 
-    $app->get("/form-venue", function() use ($app){
+    //Venue form view
+    $app->get("/form-venue", function() use ($app) {
         return $app['twig']->render('index.html.twig', array('cuisines' => Cuisine::getAll(), 'cuisine_check' => false, 'venue_check' => true));
     });
+
+    //Submit cuisine
+    $app->post('/add_cuisine', function() use ($app) {
+
+        $cuisine = new Cuisine($_POST['type']);
+        $cuisine->save();
+
+        return $app['twig']->render('index.html.twig', array('cuisines' => Cuisine::getAll(), 'cuisine_check' => false, 'venue_check' => false));
+    });
+
+    //Delete all cuisines
+    $app->post('/delete_cuisines', function() use($app) {
+        Cuisine::deleteAll();
+        return $app['twig']->render('index.html.twig', array('cuisines' => Cuisine::getAll(), 'cuisine_check' => false, 'venue_check' => false));
+    });
+
+    //Navigate to cuisine page
+    $app->get('/cuisines/{id}', function($id) use ($app) {
+        $cuisine = Cuisine::find($id);
+        return $app['twig']->render('cuisine.html.twig', array('cuisine' => $cuisine, 'venues' => $cuisine->getVenues()));
+    });
+
+
 
 
     return $app;
